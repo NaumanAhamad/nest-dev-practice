2d19b90602c16b3f4b51d0e44abf7e7197da88fc
Numaan_dev <nauman.irfan5@gmail.com>
2025-05-10 10:04:34 +0530

    Nest-new-project

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import { MessagesModule } from './messages/messages.module';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(MessagesModule);
+  const app = await NestFactory.create();
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe()
